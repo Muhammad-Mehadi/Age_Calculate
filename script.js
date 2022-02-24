b37f0a9cb0cf91eaefbcb0b3fb6ac2c31bb8dd35
mehadi <mehadi.contact@gmail.com>
2022-02-25 02:03:00 +0600

    output show,age calculate function

--- script.js
+++ script.js
@@ -1,14 +1,78 @@
 function Calculate() {
     const preDate = document.querySelector(".d1").value;
-    const nextDate = document.querySelector(".d2").value;
-    errorStatement = errorFinder(preDate, nextDate);
+    const nexDate = document.querySelector(".d2").value;
+    errorStatement = errorFinder(preDate, nexDate);
     if (errorStatement == "correct") {
-        console.log(preDate);
+        const previousDate = new Date(preDate);
+        const nextDate = new Date(nexDate);
+        console.log(previousDate);
         console.log(nextDate);
+        const previousDateYear = previousDate.getYear();
+        const previousDateMonth = previousDate.getMonth();
+        const previousDateDate = previousDate.getDate();
+        const nextDateYear = nextDate.getYear();
+        const nextDateMonth = nextDate.getMonth();
+        const nextDateDate = nextDate.getDate();
+        var age = {}
+        var ageString = {}
+        var monthAge = 0,
+            dateAge = 0;
+        var yearAge = nextDateYear - previousDateYear;
+        //get month 
+        if (nextDateMonth >= previousDateMonth) {
+            monthAge = nextDateMonth - previousDateMonth;
+        } else {
+            yearAge--;
+            monthAge = 12 + nextDateMonth - previousDateMonth;
+        }
+        // get days
+        if (nextDateDate >= previousDateDate)
+        //get days when the current date is greater  
+            dateAge = nextDateDate - previousDateDate;
+        else {
+            monthAge--;
+            dateAge = 31 + nextDateDate - previousDateDate;
+
+            if (monthAge < 0) {
+                monthAge = 11;
+                yearAge--;
+            }
+
+        }
+        age = {
+            years: yearAge,
+            months: monthAge,
+            days: dateAge
+        };
+        console.log(age.years);
+        if ((age.years > 0) && (age.months > 0) && (age.days > 0)) {
+            ageString = age.years + " years, " + age.months + " months, and " + age.days + " days old.";
+        } else if ((age.years == 0) && (age.months == 0) && (age.days > 0)) {
+            ageString = "Only " + age.days + " days old!";
+        } else if ((age.years > 0) && (age.months == 0) && (age.days == 0)) {
+            ageageString = age.years + " years old. Happy Birthday!!";
+        } else if ((age.years > 0) && (age.months > 0) && (age.days == 0)) {
+            ageageString = age.years + " years and " + age.months + " months old.";
+        } else if ((age.years == 0) && (age.months > 0) && (age.days > 0)) {
+            ageageString = age.months + " months and " + age.days + " days old.";
+        } else if ((age.years > 0) && (age.months == 0) && (age.days > 0)) {
+            ageageString = age.years + " years, and" + age.days + " days old.";
+        } else if ((age.years == 0) && (age.months > 0) && (age.days == 0)) {
+            ageageString = age.months + " months old.";
+        } else if ((age.years == 0) && (age.months == 0) && (age.days == 0)) {
+            ageString = "Welcome to Earth! <br> It's first day on Earth!";
+        } else {
+            ageString = "";
+        }
+        if (ageString != "") {
+            document.querySelector(".output").innerHTML = ageString;
+        } else {
+            document.querySelector(".output").innerHTML = "Your age input was <strong> wrong</wrong>!";
+        }
     }
 }
 
-function errorFinder(preDate, nextDate) {
+function errorFinder(preDate, nexDate) {
     if (preDate == null || preDate == "") {
         const input1 = document.querySelector(".d1");
         input1.classList.add("error-input");
@@ -17,7 +81,7 @@ function errorFinder(preDate, nextDate) {
         const input1 = document.querySelector(".d1");
         input1.classList.remove("error-input");
     }
-    if (nextDate == null || nextDate == "") {
+    if (nexDate == null || nexDate == "") {
         const input1 = document.querySelector(".d2");
         input1.classList.add("error-input");
 
@@ -26,7 +90,7 @@ function errorFinder(preDate, nextDate) {
         input1.classList.remove("error-input");
 
     }
-    if (preDate != "" && nextDate != "") {
+    if (preDate != "" && nexDate != "") {
         return "correct";
     } else {
         return "error"
